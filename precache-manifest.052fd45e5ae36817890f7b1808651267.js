e8217674fae2e783241ed9b33fcb745c9d925404
ArseniyKalabin <a.m.kalabin@gmail.com>
2020-03-01 20:30:06 +0300

    Updates

---
+++ precache-manifest.052fd45e5ae36817890f7b1808651267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827fd3a13404e5e3be34fe8485a9aa9f",
+    "url": "/xsos/index.html"
+  },
+  {
+    "revision": "faf8f375d0714bf4b46d",
+    "url": "/xsos/static/css/main.030db176.chunk.css"
+  },
+  {
+    "revision": "4f7b78e5e145a623fba4",
+    "url": "/xsos/static/js/2.b3df28e9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/xsos/static/js/2.b3df28e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf8f375d0714bf4b46d",
+    "url": "/xsos/static/js/main.69df9738.chunk.js"
+  },
+  {
+    "revision": "da78c240e2de90cfe42d",
+    "url": "/xsos/static/js/runtime-main.37550839.js"
+  }
+]);
